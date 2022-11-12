259f31a28f199fb1a100ed5bc3a28765d4bbc1ce
luisaabibi <lubiabibi@gmail.com>
2022-11-12 19:20:44 -0300

    arrumando funcoes, criando as questoes

--- jogo.py
+++ jogo.py
@@ -1,11 +1,17 @@
 from questoes import*
 from trasformabase import*
+from funcoes import* 
 
-premio = [1000, 5000, 10000, 30000, 50000, 100000, 300000, 500000, 1000000]
+dinheiro = [0, 1000, 5000, 10000, 30000, 50000, 100000, 300000, 500000, 1000000]
+rodada = 0
 pulo = 3
 ajuda = 2 
 
-magenta = '\033[35m'
+questao_para_jogo = transforma_base(questao)
+usadas = []
+numero = 0 
+
+
 
 
 print('Olá! Você está na Fortuna DesSoft e terá a opurtunidade de enriquecer!')
@@ -15,18 +21,35 @@ print('As opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar
 
 comeca = input('\naperte ENTER para continuar...')
 
-jogo = True 
+if comeca == '':
+    print ("\nO jogo já vai começar! Lá vem a Primeira questão"'\n')
+    print('Vamos começar com questões do nível FÁCIL.''\n')
+continuar = input('Aperte ENTER para começar...')
 
-for termo in valida_questoes(questao):
-
-    if termo != {}:
-        jogo = False
-        print("Possui questões invalidadas:")
-        
-while jogo == True :
-    if comeca == '':
-        jogo = False 
-        print('\nO jogo já vai começar! Lá vem a primeira questão!')
-        print('Vamos começar com questões do nível FACIL!')
-        Continuar =input('\nAperte ENTER para continuar...')
-        
+jogo = True         
+while jogo :
+    
+    # questões fáceis 
+    if rodada <= 3:
+        numero += 1 
+        questao_sorteada = sorteia_questao(questao_para_jogo, 'facil')
+        nova_questao = sorteia_questao_inedita(questao_para_jogo, 'facil', usadas)
+        questao_jogada_texto = questao_para_texto(nova_questao ,numero )
+        print(questao_jogada_texto)
+    
+    #questões médias   
+    if rodada >3 and rodada <= 6 :
+        numero +=1 
+        questao_sorteada = sorteia_questao(questao_para_jogo, 'medio')
+        nova_questao = sorteia_questao_inedita(questao_para_jogo, 'medio', usadas)
+        questao_jogada_texto = questao_para_texto(nova_questao ,numero )
+        print(questao_jogada_texto)
+    
+    if rodada > 6 and rodada <= 9 :
+        numero +=1 
+        questao_sorteada = sorteia_questao(questao_para_jogo, 'dificil')
+        nova_questao = sorteia_questao_inedita(questao_para_jogo, 'dificil', usadas)
+        questao_jogada_texto = questao_para_texto(nova_questao ,numero )
+        print(questao_jogada_texto)
+    
+    
